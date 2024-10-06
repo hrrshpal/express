2611e918f2d0cffe684766ab9745f94ea7445277
harsh <hrrshpal@gmail.com>
2024-10-06 20:13:08 +0530

    created an express server

---
+++ intro-to-express/server/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+const port = 5000
+
+app.get("/status",(req, res) => {
+    res.send("server is alive")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+})
