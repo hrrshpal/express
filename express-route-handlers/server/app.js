18233d0e6b6e176084320782e7ef02c46c86230a
harsh <hrrshpal@gmail.com>
2024-10-07 10:12:07 +0530

    phase 1 completed

---
+++ express-route-handlers/server/app.js
@@ -0,0 +1,45 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.json())
+
+app.use((req,res,next)=>{
+  console.log("Request Body:", req.body)
+  next()
+})
+
+app.post("/", (req,res)=>{
+  res.send("Received Request")
+})
+
+// DO NOT MODIFY
+if (require.main === module) {
+  const port = 8000;
+  app.listen(port, () => console.log('Server is listening on port', port));
+} else {
+  module.exports = app;
+}
